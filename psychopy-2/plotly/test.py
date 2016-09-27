16c60dc0a1d333c1cb303a4c520926672b326f36
Momchil Tomov <momchil@memsql.com>
2016-09-26 19:44:35 -0700

    getNextFile but not quite...  need mysql

--- psychopy-2/plotly/test.py
+++ psychopy-2/plotly/test.py
@@ -1,14 +1,42 @@
 from flask import Flask, render_template
+import random
 import os
+import json
 
 # start Flask app
 #
 app = Flask(__name__)
 
+dataFilename = None
+dataFile = None
+dataDir = os.path.join('..', 'data')
+
+
+def getLatestFile():
+    global dataFilename
+    global dataFile
+    files = [f for f in os.listdir(dataDir) if f.endswith('.wtf')]
+    dataFilename = files[-1]
+    print 'last file = ', dataFilename
+    dataFile = open(os.path.join(dataDir, dataFilename), 'r')
 
 @app.route('/')
 def index():
+    getLatestFile()
     return render_template('test.html')
+
+@app.route('/get_next', methods=['GET'])
+def get_next():
+    global dataFile
+    lines = []
+    print ' --------- GET ! ---------- '
+    while True:
+        line = dataFile.readline()
+        if not line:
+            break # no more lines... for now
+        lines.append(line)
+        print '    line = ', line
+    return json.dumps(lines)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
