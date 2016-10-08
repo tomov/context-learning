3f5fa48c87719bf50840880d4a23bafcf21676c2
Momchil Tomov <momchil@memsql.com>
2016-10-07 23:35:05 -0400

    more plots -- model.keys, model.pred

--- model/analyze.m
+++ model/analyze.m
@@ -19,6 +19,7 @@ human_correct_all_runs = [];
 model_correct_all_runs = [];
 
 model.keys = {}; % equivalent to response.keys but for the model (i.e. the responses)
+model.pred = [];
 
 for who = unique(participant)'
     for condition = unique(contextRole)'
@@ -39,26 +40,41 @@ for who = unique(participant)'
             c = contextId(which_train) + 1;
             r = strcmp(sick(which_train), 'Yes');
             [choices, P_n, ww_n, P, ww] = train(x, c, r, false);
-            model_choices = choices > 0.5;
+            
+            model_choices = choices > rand;
             model_response_keys = {};
             model_response_keys(model_choices) = {'left'};
             model_response_keys(~model_choices) = {'right'};
             model.keys(which_train) = model_response_keys;
+            model.pred(which_train) = choices;
+            
+            % See what the model predicts for the test trials of that run
+            %
+            test_cues = cueId(which_test);
+            test_N = length(test_cues); % # of trials
+            D = 3; % # of stimuli
+            test_x = zeros(test_N, D);
+            test_x(sub2ind(size(test_x), 1:test_N, test_cues' + 1)) = 1;
+            test_c = contextId(which_test) + 1;
+            [test_choices] = test(test_x, test_c, P_n, ww_n);
+
+            model_test_choices = test_choices > rand;
+            model_test_response_keys = {};
+            model_test_response_keys(model_test_choices) = {'left'};
+            model_test_response_keys(~model_test_choices) = {'right'};
+            model.keys(which_test) = model_test_response_keys;
+            model.pred(which_test) = test_choices;
             
             % Get the subject's responses too.
             %
             resp = response.keys(which_train);
-            human_choices = strcmp(resp, 'left'); % sick == 1
-            
-            % Keep track of correct responses
-            %
-            human_correct_all_runs = [human_correct_all_runs; (human_choices == r)'];
-            model_correct_all_runs = [model_correct_all_runs; (model_choices == r)'];
+            human_choices = strcmp(resp, 'left'); % sick == 1            
         end
     end
 end
 
 model.keys = model.keys';
+model.pred = model.pred';
 
 %% Do some plotting
 %
@@ -150,7 +166,7 @@ legend({'x_1c_1', 'x_1c_2', 'x_2c_1', 'x_2c_2'});
 
 
 %
-% Choice probabilities in test phase
+% Choice probabilities in test phase for SUBJECTS
 % This is the final figure we care about
 %
 
@@ -182,4 +198,76 @@ subplot(2, 3, 4);
 barweb(Ms, SEMs, 1, contexts, 'Subject P(choose sick) in test phase (main figure)');
 ylabel('Sick probability');
 legend({'x_1c_1', 'x_1c_3', 'x_3c_1', 'x_3c_3'});
+
+
+%
+% Choice probabilities in test phase for MODEL (based on the actual
+% decisions the model made)
+% This is the final figure we care about
+%
+
+Ms = [];
+SEMs = [];
+for context = contexts
+    which = isTrain == 0 & strcmp(contextRole, context);
+    
+    
+    x1c1 = strcmp(model.keys(which & cueId == 0 & contextId == 0), 'left');
+    x1c2 = strcmp(model.keys(which & cueId == 0 & contextId == 2), 'left');
+    x2c1 = strcmp(model.keys(which & cueId == 2 & contextId == 0), 'left');
+    x2c2 = strcmp(model.keys(which & cueId == 2 & contextId == 2), 'left');
+    
+    assert(length(x1c1) == roundsPerContext * subjects);
+    assert(length(x1c2) == roundsPerContext * subjects);
+    assert(length(x2c1) == roundsPerContext * subjects);
+    assert(length(x2c2) == roundsPerContext * subjects);
+
+    M = mean([x1c1 x1c2 x2c1 x2c2]);
+    SEM = std([x1c1 x1c2 x2c1 x2c2]) / sqrt(length(x1c1));
+%    M = [mean(x1c1) mean(x1c2) mean(x2c1) mean(x2c2)];
+%    SEM = [std(x1c1) / sqrt(length(x1c1)) std(x1c2) / sqrt(length(x1c2)) std(x2c1) / sqrt(length(x2c1)) std(x2c2) / sqrt(length(x2c2))];
+    Ms = [Ms; M];
+    SEMs = [SEMs; SEM];
+end
+    
+subplot(2, 3, 5);
+
+barweb(Ms, SEMs, 1, contexts, 'Model P(choose sick) in test phase (main figure)');
+ylabel('Sick probability');
+legend({'x_1c_1', 'x_1c_3', 'x_3c_1', 'x_3c_3'});
+
+
+%
+% TRUE Choice probabilities in test phase for MODEL
+%
+
+Ms = [];
+SEMs = [];
+for context = contexts
+    which = isTrain == 0 & strcmp(contextRole, context);
+    
+    x1c1 = model.pred(which & cueId == 0 & contextId == 0);
+    x1c2 = model.pred(which & cueId == 0 & contextId == 2);
+    x2c1 = model.pred(which & cueId == 2 & contextId == 0);
+    x2c2 = model.pred(which & cueId == 2 & contextId == 2);
+    
+    assert(length(x1c1) == roundsPerContext * subjects);
+    assert(length(x1c2) == roundsPerContext * subjects);
+    assert(length(x2c1) == roundsPerContext * subjects);
+    assert(length(x2c2) == roundsPerContext * subjects);
+
+    M = mean([x1c1 x1c2 x2c1 x2c2]);
+    SEM = std([x1c1 x1c2 x2c1 x2c2]) / sqrt(length(x1c1));
+%    M = [mean(x1c1) mean(x1c2) mean(x2c1) mean(x2c2)];
+%    SEM = [std(x1c1) / sqrt(length(x1c1)) std(x1c2) / sqrt(length(x1c2)) std(x2c1) / sqrt(length(x2c1)) std(x2c2) / sqrt(length(x2c2))];
+    Ms = [Ms; M];
+    SEMs = [SEMs; SEM];
+end
+    
+subplot(2, 3, 6);
+
+barweb(Ms, SEMs, 1, contexts, 'Model P(choose sick) in test phase (true probs)');
+ylabel('Sick probability');
+legend({'x_1c_1', 'x_1c_3', 'x_3c_1', 'x_3c_3'});
+
 
